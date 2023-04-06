18b058aafc12795b40b15e12211732a66c1218bc
bujosa <davidbujosa@gmail.com>
2023-04-06 03:18:07 -0400

    feat: implement dog provider

---
+++ lib/core/providers/dog_provider.dart
@@ -0,0 +1,11 @@
+import 'package:ancha/core/models/dog.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DogProvider extends ChangeNotifier {
+  Stream<List<Dog>> getDogs() => FirebaseFirestore.instance
+      .collection('dogs')
+      .snapshots()
+      .map((snapshot) =>
+          snapshot.docs.map((doc) => Dog.fromJSON(doc.data())).toList());
+}
